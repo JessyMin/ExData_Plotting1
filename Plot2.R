8b2eeebbe49dd9ed0d02787b369f62af4042807f
JessyMin <uxinsight@naver.com>
2017-04-11 23:37:52 +0900

    remove blanks

--- Plot2.R
+++ Plot2.R
@@ -5,7 +5,7 @@ rawdata = read.table("household_power_consumption.txt", header=TRUE, sep=";",
                      stringsAsFactors=FALSE)
 
 #Subset data with Date
-rawdata$Date = as.Date(rawdata$Date, format = "%d/%m/%Y")
+rawdata$Date = as.Date(rawdata$Date, format="%d/%m/%Y")
 data = filter(rawdata, Date == "2007-02-01"|Date == "2007-02-02")
 
 #Set column classes to numeric
@@ -13,16 +13,16 @@ data[,3:9] = lapply(data[,3:9], function(x) as.numeric(x))
 
 #Merge Date/Time columns to POSIXlt 
 data$date_time = paste(data$Date, data$Time)
-data$date_time = strptime(data$date_time, format = "%Y-%m-%d %H:%M:%S")
+data$date_time = strptime(data$date_time, format="%Y-%m-%d %H:%M:%S")
 
 
 #Set graphic device to PNG file
-png(filename = "plot2.png", width = 480, height = 480)
+png(filename="plot2.png", width=480, height=480, units="px")
 Sys.setlocale("LC_TIME", "C")  #solving language problem
 
 #Make and anotate histogram
-with(data, plot(date_time, Global_active_power, type = "l",
-                xlab = "", ylab = "Global Active Power (kilowatts)"))
+with(data, plot(date_time, Global_active_power, type="l",
+                xlab="", ylab="Global Active Power (kilowatts)"))
 
 #Save into PNG file
 dev.off()
