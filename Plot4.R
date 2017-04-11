c55659088044d4d3446212a7dda38b0d76261c2e
JessyMin <uxinsight@naver.com>
2017-04-11 23:38:55 +0900

    remove blanks

--- Plot4.R
+++ Plot4.R
@@ -6,42 +6,42 @@ rawdata = read.table("household_power_consumption.txt", header=TRUE, sep=";",
 
 #Subset data with Date
 rawdata$Date = as.Date(rawdata$Date, format = "%d/%m/%Y")
-data = filter(rawdata, Date == "2007-02-01"|Date=="2007-02-02")
+data = filter(rawdata, Date == "2007-02-01"|Date == "2007-02-02")
 
 #Set column classes to numeric
 data[,3:9] = lapply(data[,3:9], function(x) as.numeric(x))
 
 #Merge Date/Time columns to POSIXlt 
 data$date_time = paste(data$Date, data$Time)
-data$date_time = strptime(data$date_time, format = "%Y-%m-%d %H:%M:%S")
+data$date_time = strptime(data$date_time, format="%Y-%m-%d %H:%M:%S")
 
 
 #Set graphic device to PNG file
-png(filename = "plot4.png", width = 480, height = 480)
+png(filename="plot4.png", width=480, height=480, units="px")
 Sys.setlocale("LC_TIME", "C")  #solving language problem
 
 par(mfrow=c(2,2))
 
 #Make 1st plot
-with(data, plot(date_time, Global_active_power, type = "l",
-                xlab = "", ylab = "Global Active Power"))
+with(data, plot(date_time, Global_active_power, type="l",
+                xlab="", ylab="Global Active Power"))
 
 #Make 2nd plot
-with(data, plot(date_time, Voltage, type = "l",
-                xlab = "datetime", ylab = "Voltage"))
+with(data, plot(date_time, Voltage, type="l",
+                xlab="datetime", ylab="Voltage"))
 
 #Make 3rd plot
-with(data, plot(date_time, Sub_metering_1, type = "l",
-                xlab = "", ylab = "Energy sub metering", ylim = c(0,38)))
-with(data, lines(date_time, Sub_metering_2, col = 2))
-with(data, lines(date_time, Sub_metering_3, col = 4))
+with(data, plot(date_time, Sub_metering_1, type="l",
+                xlab="", ylab="Energy sub metering", ylim=c(0,38)))
+with(data, lines(date_time, Sub_metering_2, col=2))
+with(data, lines(date_time, Sub_metering_3, col=4))
 
 legend("topright", c("sub_metering_1", "sub_metering_2", "sub_metering_3"), 
-       lwd =  1, col = c(1,2,4), bty = "n")
+       lwd=1, col=c(1,2,4), bty="n")
 
 #Make 4th plot
-with(data, plot(date_time, Global_reactive_power, type = "l",
-                xlab = "datetime"))
+with(data, plot(date_time, Global_reactive_power, type="l",
+                xlab="datetime"))
 
 #Save into PNG file
 dev.off()
